a8a287888a15af724b1c510891a43db874105cc4
ahmedbatty <ahmedbatty@gmail.com>
2019-11-06 21:34:28 +0500

    Updated admin pages

--- projects/admin.py
+++ projects/admin.py
@@ -2,5 +2,32 @@ from django.contrib import admin
 
 from .models import AboutPerson, Project
 
-admin.site.register(AboutPerson)
-admin.site.register(Project)
+
+class AboutPersonAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,
+         {'fields': ['name', 'about', 'image', 'about_image_thumbnail', 'resume', 'is_active']}),
+    ]
+    readonly_fields = ['about_image_thumbnail']
+
+    list_display = ('id', 'name', 'is_active')
+    list_display_links = ('name',)
+    search_fields = ('name',)
+    ordering = ('id',)
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,
+         {'fields': ['title', 'detail', 'image', 'project_image_thumbnail', 'date', 'is_active']}),
+    ]
+    readonly_fields = ['project_image_thumbnail']
+
+    list_display = ('id', 'title', 'summary', 'date_pretty', 'is_active')
+    list_display_links = ('title',)
+    search_fields = ('title', 'detail',)
+    ordering = ('id',)
+
+
+admin.site.register(AboutPerson, AboutPersonAdmin)
+admin.site.register(Project, ProjectAdmin)
--- projects/models.py
+++ projects/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.utils.html import format_html
 
 
 class AboutPerson(models.Model):
     name = models.CharField(max_length=100)
-    picture = models.ImageField(upload_to='images/', blank=True)
+    image = models.ImageField(upload_to='images/', blank=True)
     resume = models.FileField(upload_to='resume/')
     about = models.TextField(max_length=5000)
     is_active = models.BooleanField(default=True)
@@ -11,10 +12,16 @@ class AboutPerson(models.Model):
     def __str__(self):
         return self.name
 
+    def about_image_thumbnail(self):
+        img = f'{self.image.url}' if self.image else 'No Thumbnail!'
+        style = ''
+
+        return format_html(f'<img style="{style}" src="{img}" alt="{img}" width="200" height="200">')
+
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
-    picture = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/')
     date = models.DateField()
     detail = models.TextField(max_length=3000)
     is_active = models.BooleanField(default=True)
@@ -27,3 +34,9 @@ class Project(models.Model):
 
     def date_pretty(self):
         return self.date.strftime('%B, %Y')
+
+    def project_image_thumbnail(self):
+        img = f'{self.image.url}' if self.image else 'No Thumbnail!'
+        style = ''
+
+        return format_html(f'<img style="{style}" src="{img}" alt="{img}" width="560" height="360">')
