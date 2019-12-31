f40cbd44725477ac7f3183cff95f18c14b5ee147
Ahmed Muqaddas <ahmedbatty@gmail.com>
2019-12-31 19:44:16 +0500

    Edited media path

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -128,8 +128,8 @@ else:
         os.path.join(BASE_DIR, 'static'),
     )
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CKEDITOR_UPLOAD_PATH = "ckuploads/"
 
